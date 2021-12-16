ade8c7b30174d6a546750f7d1c8b6893a0fc084d
vitor-mat <vitormateusalves123@gmail.com>
2021-12-16 10:46:26 -0300

    projeto limpo

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  let name = "world"
+  const titulo = <h1>Hello, {name}</h1>
+  return (
+    <div className="App">
+      		{titulo}
+    </div>
+  )
+}
+
+export default App;
