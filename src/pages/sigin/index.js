755af7d0128dc51fc8ee7e4d2bbea060c0a8f9dc
vitor-mat <vitormateusalves123@gmail.com>
2021-12-24 11:55:30 -0300

    validacao dos campos de cadastro feito

--- src/pages/sigin/index.js
+++ src/pages/sigin/index.js
@@ -35,6 +35,23 @@ export const SignInPage = () => {
 
     const formValidation = () => {
 
+        if(!userNameData.length) return alert("Error: O campo com o nome de usuário não pode estar vazio!")
+
+        if(!emailData.length) return alert("Error: O campo de email não pode estar vazio!")
+
+        if(!passwordData.length) return alert("Error: O campo de senha não pode estar vazio!")
+
+        if(!passwordConfirmData.length) return alert("Error: O campo de confirmação de senha não pode estar vazio!")
+
+        if(
+            emailData.indexOf(".com") === -1 ||
+            emailData.indexOf("@") === -1 ||
+            emailData.indexOf(" ") !== -1 ||
+            emailData.length < 8 ||
+            emailData[emailData.indexOf("@")-1] === undefined ||
+            emailData[emailData.indexOf("@")+1] === undefined ||
+            emailData[emailData.indexOf("@")+1] === "."
+            ) return alert("Error: Este email está inválido!")
     }
 
     return (
@@ -51,8 +68,8 @@ export const SignInPage = () => {
                         <input type="text" placeholder="Nome de usuário" onChange={(e) => handleInputData(e, userNameData, setUserNameData)} />
                         <input type="email" placeholder="Email" onChange={(e) => handleInputData(e, emailData, setEmailData)} />
                         <input type="password" placeholder="Senha" onChange={(e) => handleInputData(e, passwordData, setPasswordData)} />
-                        <input type="password" placeholder="Confirme a senha" onChange={(e) => handleInputData(e, passwordConfirmData, setPasswordConfirmDat)} />
-                        <button onClick={() => sigInFirebase()}>Cadastrar</button>
+                        <input type="password" placeholder="Confirme a senha" onChange={(e) => handleInputData(e, passwordConfirmData, setPasswordConfirmData)} />
+                        <button onClick={formValidation}>Cadastrar</button>
                     </div>
                 </div>
                 <div id="create-accout-div">
