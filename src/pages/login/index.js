5722b342965fa39952b2cb21b1123033d5a64c43
vitor-mat <vitormateusalves123@gmail.com>
2021-12-24 12:10:24 -0300

    validacao dos campos de formulario de entrada feito

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './style.scss'
 
 export const LoginPage = () => {
+
+    let [emailData, setEmailData] = useState("")
+    let [passwordData, setPasswordData] = useState("")
+
+
+    const handleInputData = (e, stateVariable, stateFunction) => {
+        stateFunction(stateVariable = e.target.value)
+    }
+
+
+    const formValidation = () => {
+
+        if(!emailData.length) return alert("Error: O campo de email não pode estar vazio!")
+
+        if(!passwordData.length) return alert("Error: O campo de senha não pode estar vazio!")
+
+        if(
+            emailData.indexOf(".com") === -1 ||
+            emailData.indexOf("@") === -1 ||
+            emailData.indexOf(" ") !== -1 ||
+            emailData.length < 8 ||
+            emailData[emailData.indexOf("@")-1] === undefined ||
+            emailData[emailData.indexOf("@")+1] === undefined ||
+            emailData[emailData.indexOf("@")+1] === "."
+            ) return alert("Error: Este email está inválido!")
+        
+    }
+
     return (
         <div className="container">
             <header>
@@ -14,9 +42,8 @@ export const LoginPage = () => {
                         <h2>Entrar</h2>
                     </div>
                     <div id="login-inputs-div">
-                        <input type="email" placeholder="Email" />
-                        <input type="password" placeholder="Password" />
-                        <button>Entrar</button>
+                        <input type="email" placeholder="Email" onChange={(e) => handleInputData(e, emailData, setEmailData)} />
+                        <input type="password" placeholder="Senha" onChange={(e) => handleInputData(e, passwordData, setPasswordData)} />                        <button>Entrar</button>
                     </div>
                 </div>
                 <div id="create-accout-div">
